6fd3595d68a96f18dc5b49102b165d6ddb808e3f
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-10 16:30:19 +0700

    Update Relation Model Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Task extends Model
 {
@@ -11,8 +12,17 @@ class Task extends Model
 
     protected $guarded = ['id'];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function taskAssignments()
     {
         return $this->hasMany(TaskAssignments::class);
+    }
+    public function asigne()
+    {
+        return $this->belongsToMany(User::class);
     }
 }
